ec56d4e8d964b34f9caa45c469a4b0a14f03277c
abehnan <abehnan@gmail.com>
2017-09-28 17:08:51 -0400

    decrypt working

--- MyDes.java
+++ MyDes.java
@@ -10,6 +10,132 @@ public class MyDes {
 				temp[j][i] = m[i][j];
 		return temp;
 	}
+	
+	private static char[] encrypt(int key, String input) {
+		
+		char[] message = input.toCharArray();
+		int firstDigit = key / 10;
+		int row;
+		int maxLen = (firstDigit + 1) * 1000;
+		char[] A = new char[maxLen];
+		char[] B = new char[maxLen];
+		char[] C = new char[maxLen];
+		char[][] T = new char[firstDigit][1000];
+		char[][] T2 = new char[firstDigit + 1][1000];
+		int len = message.length;
+
+		// debug
+		System.out.println("~~~~~~~~~~~~~~~~  Encryption Starting  ~~~~~~~~~~~~~~~~");
+		System.out.println("key: " + key);
+		System.out.println("string: " + String.valueOf(message));
+
+		row = 0;
+		
+		// populate T with the message
+		for (int i = 0; i < len; i++) {
+			if (i > 0 && i % 1000 == 0)
+				row++;
+			T[row][i % 1000] = message[i];
+
+		}
+
+		// copy transpose of 2D array T into array A
+		for (int j = 0; j < 1000; j++) {
+			for (int i = 0; i < firstDigit; i++) {
+				A[(j * firstDigit) + i] = T[i][j];
+			}
+		}
+		System.out.println("A is: " + String.valueOf(A));
+
+
+		// apply Caesar cipher to A and store into B
+		for (int i = 0; i < maxLen; i++) {
+			B[i] = (char) (A[i] + key);
+		}
+		System.out.println("B is: " + String.valueOf(B));
+
+		// populate second 2D array
+		row = 0;
+		for (int i = 0; i < maxLen; i++) {
+			if (i > 0 && i % 1000 == 0)
+				row++;
+			T2[row][i % 1000] = B[i];
+		}
+
+		// copy transpose of 2D array T2 into array C
+		for (int j = 0; j < 1000; j++) {
+			for (int i = 0; i < firstDigit + 1; i++) {
+				C[j * (firstDigit + 1) + i] = T2[i][j];
+			}
+		}
+		
+		System.out.println("~~~~~~~~~~~~~~~~  Encryption Ending  ~~~~~~~~~~~~~~~~");
+		return C;
+	}
+	
+	public static char[] decrypt(int key, String input) {
+		
+		char[] C = input.toCharArray();
+		int firstDigit = key / 10;
+		int row, col;
+		int maxLen = (firstDigit+1) * 1000;
+		char[] B = new char[maxLen];
+		char[] A = new char[maxLen];
+		char[] result = new char[maxLen];
+		char[][] T = new char[1000][firstDigit];
+		char[][] T2 = new char[1000][firstDigit + 1];
+		// char[][] T2 = new char[firstDigit + 1][1000];
+		int len = C.length;
+
+		// debug
+		System.out.println("~~~~~~~~~~~~~~~~  Decryption Starting  ~~~~~~~~~~~~~~~~");
+		System.out.println("key: " + key);
+		System.out.println("C: " + String.valueOf(C));
+
+		row = 0;
+		// populate T2 with C
+		for (int i = 0; i < maxLen; i++) {
+			if (i > 0 && i % (firstDigit + 1) == 0)
+				row++;
+			T2[row][i % (firstDigit + 1)] = C[i];
+
+		}
+
+		// copy transpose of 2D array T2 into array B
+		for (int j = 0; j < firstDigit + 1; j++) {
+			for (int i = 0; i < 1000; i++) {
+				B[j*1000 + i] = T2[i][j];
+			}
+		}
+		System.out.println("B is: " + String.valueOf(B));
+
+
+		// apply the reverse Caesar cipher to B and store into A
+		for (int i = 0; i < maxLen; i++) {
+			A[i] = (char) (B[i] - key);
+		}
+		System.out.println("A is: " + String.valueOf(A));
+
+		// populate second T4 with E 
+		row = 0;
+		for (int i = 0; i < len; i++) {
+			if (i > 0 && i % firstDigit == 0)
+				row++;
+			if (i == 1000)
+				break;
+			T[row][i % firstDigit] = A[i];
+		}
+
+		// copy transpose of 2D array T4 into result
+		for (int j = 0; j < firstDigit; j++) {
+			for (int i = 0; i < 1000; i++) {
+				result[j*1000 + i] = T[i][j];
+			}
+		}
+		
+		System.out.println("~~~~~~~~~~~~~~~~  Decryption Ending  ~~~~~~~~~~~~~~~~");
+		return result;
+	}
 
 	public static void main(String[] args) {
 
@@ -34,112 +160,14 @@ public class MyDes {
 			System.exit(0);
 		}
 
-		char[] message = input.toCharArray();
-		int firstDigit = key / 10;
-		int row, col;
-		int maxLen = (firstDigit + 1) * 1000;
-		char[] A = new char[maxLen];
-		char[] B = new char[maxLen];
-		char[] C = new char[maxLen];
-		char[] D = new char[maxLen];
-		char[] E = new char[maxLen];
-		char[][] T = new char[firstDigit][1000];
-		char[][] T2 = new char[firstDigit + 1][1000];
-		char[][] T3 = new char[firstDigit + 1][1000];
-		char[][] T4 = new char[firstDigit][1000];
-		int len = message.length;
-
-		// debug
-		System.out.println("firstDigit: " + firstDigit);
-		System.out.print("messageArray: ");
-		for (int i = 0; i < input.length(); i++) {
-			System.out.print(message[i]);
-		}
-		System.out.println();
-
-		row = 0;
-		col = 0;
-		// populate T with the message
-		for (int i = 0; i < len; i++) {
-			if (i > 0 && i % 1000 == 0)
-				row++;
-			T[row][i % 1000] = message[i];
-
-		}
-
-		// debug
-		// System.out.print("T: " + Arrays.deepToString(T));
-		// System.out.println();
-
-		// copy transpose of 2D array T into array A
-		for (int j = 0; j < 1000; j++) {
-			for (int i = 0; i < firstDigit; i++) {
-				A[(j * firstDigit) + i] = T[i][j];
-			}
-		}
-
-		System.out.print("A is: ");
-		for (int i =0; i < A.length; i++) {
-			System.out.print(A[i]);
-		}
-		System.out.println();
-
-		// apply Caesar cipher to A and store into B
-		for (int i = 0; i < len; i++) {
-			B[i] = (char) (A[i] + key);
-		}
-
-		System.out.print("B is: ");
-		for (int i =0; i < B.length; i++) {
-			System.out.print(B[i]);
-		}
-		System.out.println();
-
-		// populate second 2D array
-		row = 0;
-		for (int i = 0; i < B.length; i++) {
-			if (i > 0 && i % 1000 == 0)
-				row++;
-			T2[row][i % 1000] = B[i];
-		}
-
-		// copy transpose of 2D array T2 into array C
-		for (int j = 0; j < 1000; j++) {
-			for (int i = 0; i < firstDigit + 1; i++) {
-				C[j * (firstDigit + 1) + i] = T2[i][j];
-			}
-		}
-
-		System.out.print("The encrypted string C is: ");
-		for (int i =0; i < C.length; i++) {
-			System.out.print(C[i]);
-		}
-		System.out.println();
-
-		// now we have to decrypt it.
-
-		// populate T3 with C
-		row = 0;
-		for (int i = 0; i < B.length; i++) {
-			if (i > 0 && i % 1000 == 0)
-				row++;
-			T3[row][i % 1000] = C[i];
-		}
-
-		// copy transpose of 2D array T3 into array D
-		for (int j = 0; j < 1000; j++) {
-			for (int i = 0; i < firstDigit + 1; i++) {
-				D[j * (firstDigit + 1) + i] = T3[i][j];
-			}
-		}
+		char[] encrypted = encrypt(key, input);
+		String encryptedString = String.valueOf(encrypted);
+		System.out.println("The encryped message C is: " + encryptedString);
 		
-		// apply Caesar cipher to D and store into E
-				for (int i = 0; i < len; i++) {
-					B[i] = (char) (A[i] + key);
-				}
+		char[] decrypted = decrypt(key, encryptedString);
+		String decryptedString = String.valueOf(decrypted);
+		System.out.println("The decrypted message is:" + decryptedString);
 		
-		
-
 		reader.close();
 		System.exit(0);
 	}
